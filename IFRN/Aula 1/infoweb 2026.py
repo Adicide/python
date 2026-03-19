dde058d00391573547d6399c7a499a3170d22e94
Lucas Gabriel <lucastavaresrego@gmail.com>
2026-03-19 10:24:35 -0300

    Add files via upload

---
+++ IFRN/Aula 1/infoweb 2026.py
@@ -0,0 +1,20 @@
+print("Infoweb 2026")
+
+DIATINF = 10000000
+DIAREN = 0
+DIACON = -119
+DIACIN = -9999999
+
+if DIATINF >= DIAREN:
+    print("DIATINF MAIORAL 🤪🤪")
+    print("RIP DIAREN")
+
+if DIACON <= DIATINF:
+    print("DIATINF MAIORAL 🤪🤪")
+    print("RIP DIACON")
+
+if DIACIN <= DIATINF:
+    print("DIATINF MAIORAL 🤪🤪")
+    print("RIP DIACIN")
+
+    
