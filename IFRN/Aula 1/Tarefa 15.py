5ab8ba36b0a18c82501523d41fe067ca15b18efe
Lucas Gabriel <lucastavaresrego@gmail.com>
2026-03-24 21:48:41 -0300

    Add files via upload

---
+++ IFRN/Aula 1/Tarefa 15.py
@@ -0,0 +1,7 @@
+# 15. Declare três variáveis x, y e z com os valores 1, 2 e 3 respectivamente. Exiba a soma das três variáveis.
+
+x = 1
+y = 2
+z = 3
+
+print(x + y + z)
---
+++ IFRN/Aula 1/Tarefa 19.py
@@ -0,0 +1,5 @@
+# 19. Atribua o valor 100 a uma variável pontos. Em seguida, some 50 a essa variável e exiba o resultado.
+
+pontos = 100
+
+print(pontos + 50)
