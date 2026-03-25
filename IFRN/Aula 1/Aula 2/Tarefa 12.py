23cf7ebbffad75201550ab70aef4e904973514b2
Lucas Gabriel <lucastavaresrego@gmail.com>
2026-03-24 22:03:31 -0300

    Delete IFRN/Aula 1/Aula 2 directory

--- IFRN/Aula 1/Aula 2/Tarefa 12.py
+++
@@ -1,7 +0,0 @@
-# 12. Declare duas variáveis, cidade e estado, e exiba uma mensagem no formato:
-# Eu moro em Natal, RN.
-
-Cidade = 'Natal'
-Estado = 'RN'
-
-print(f"Eu moro em {Cidade}, {Estado}")
