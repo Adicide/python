a6920161d060ef73354be140550f529ea7a7fd14
Lucas Gabriel <lucastavaresrego@gmail.com>
2026-03-24 21:59:11 -0300

    Delete IFRN/Aula 1/Tarefa 6.py

--- IFRN/Aula 1/Tarefa 6.py
+++
@@ -1,7 +0,0 @@
-# 6. Escreva um programa que exiba, utilizando um único comando print, as palavras "Python", "é" e "incrível" separadas por espaço.
-
-a= 'Python'
-b= 'é'
-c= 'incrível'
-
-print(f"{a} {b} {c}")
