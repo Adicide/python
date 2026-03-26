4563c95b80f0c4fc04322eac1a66f36676bdc679
Lucas Gabriel <lucastavaresrego@gmail.com>
2026-03-26 00:36:12 -0300

    Delete IFRN/Aula 2 directory

--- IFRN/Aula 2/Tarefa 11.py
+++
@@ -1,5 +0,0 @@
-# 11. Declare uma variável chamada nome com o valor "Maria" e exiba o conteúdo dessa variável.
-
-Nome = 'Maria'
-
-print(Nome)
--- IFRN/Aula 2/Tarefa 13.py
+++
@@ -1,5 +0,0 @@
-# 13. Declare uma variável ano_nascimento com o valor 2010 e exiba uma mensagem informando o ano de nascimento.
-
-ano_nascimento = 2010
-
-print(f"O ano de nascimento é: {ano_nascimento}")
