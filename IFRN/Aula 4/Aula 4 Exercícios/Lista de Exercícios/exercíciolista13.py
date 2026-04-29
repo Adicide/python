427a3b0f9069632fef14d4e296bd3ea27bb01de3
Lucas Gabriel <lucastavaresrego@gmail.com>
2026-04-29 00:29:29 -0300

    Delete IFRN/Aula 4/Aula 4 Exercícios/Lista de Exercícios directory

--- IFRN/Aula 4/Aula 4 Exercícios/Lista de Exercícios/exercíciolista13.py
+++
@@ -1,9 +0,0 @@
-# 13. Avalie manualmente o resultado da expressão abaixo, passo a passo, e depois confirme no interpretador.
-
-# G < 0 and G > -10
-
-G = -3
-
-print(G < 0 and G > -10)
-
-# Resultado: True and True => True
--- IFRN/Aula 4/Aula 4 Exercícios/Lista de Exercícios/exercíciolista4.py
+++
@@ -1,9 +0,0 @@
-# 4. Qual é o resultado da expressão abaixo? Avalie manualmente e depois confirme no interpretador Python.
-
-# not D
-
-D = True
-
-print(not D)
-
-# Resultado = False
